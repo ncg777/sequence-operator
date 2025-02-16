8f705eb2ed65a1f7282162f5bf554a3fea880234
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2025-02-15 22:23:44 -0500

    ...

--- components.d.ts
+++ components.d.ts
@@ -7,8 +7,5 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    RhythmHadamard: typeof import('./src/components/RhythmHadamard.vue')['default']
-    RhythmIV: typeof import('./src/components/RhythmIV.vue')['default']
-    RhythmSelector: typeof import('./src/components/RhythmSelector.vue')['default']
   }
 }
